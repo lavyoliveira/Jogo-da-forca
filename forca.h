19cf93203a159525d45daadf6499191be0573a0a
Guilherme Santos <guilherme.ssilva@protonmail.com>
2022-10-08 21:49:37 -0300

    Modificado arquivo "forca.h"

--- forca.h
+++ forca.h
@@ -1,7 +1,18 @@
+#ifndef FORCA_H_INCLUDED
+#define FORCA_H_INCLUDED
+
 void abertura();
+
 void chuta();
+
 int jaChutou(char letra);
+
 void desenhaForca();
+
 void escolhePalavra();
+
 int acertou();
+
 int enforcou();
+
+#endif // FORCA_H_INCLUDED
